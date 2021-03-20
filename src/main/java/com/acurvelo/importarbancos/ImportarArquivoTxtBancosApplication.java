c1bcf0fb9b6dfed70fd079bf099281f9f91f2fda
Alexandre Oliveira Curvelo <allexolic@gmail.com>
2021-03-20 17:11:27 -0300

    Project created

---
+++ src/main/java/com/acurvelo/importarbancos/ImportarArquivoTxtBancosApplication.java
@@ -0,0 +1,13 @@
+package com.acurvelo.importarbancos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportarArquivoTxtBancosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportarArquivoTxtBancosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acurvelo/importarbancos/ImportarArquivoTxtBancosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acurvelo.importarbancos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportarArquivoTxtBancosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
