fe0d3d774d153f727bea14ef3c9d011a6dd7298d
Alexandre Oliveira Curvelo <allexolic@gmail.com>
2021-03-20 17:55:33 -0300

    model Banco created

---
+++ src/main/java/com/acurvelo/importarbancos/model/Banco.java
@@ -0,0 +1,29 @@
+package com.acurvelo.importarbancos.model;
+
+public class Banco {
+
+	private String codigo;
+	private String nome;
+	private String webSite;
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getWebSite() {
+		return webSite;
+	}
+	public void setWebSite(String webSite) {
+		this.webSite = webSite;
+	}
+	
+	
+}
